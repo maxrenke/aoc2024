9a44451e1144d8707f46651f419d1cee83703d8f
Max Renke <maxrenke@gmail.com>
2024-12-14 22:56:26 -0800

    cleaning up, getting ready for 2024

--- day01/day01.py
+++ day01/day01.py
@@ -7,22 +7,4 @@ else:
     print("No file name provided.")
     sys.exit(1)
 
-with open(file_name, 'r') as file:
-    values = []
-    for line in file:
-        # Process each line here
-        digits = ''.join(filter(str.isdigit, line))
-        length = len(digits)
-        
-        first = digits[0]
-        last = digits[length - 1]
-
-        value = (str(first) + str(last))
-        values.append(value)
-
-sum = 0
-for v in values:
-    #print(v)
-    sum += int(v)
-
-print(sum) 
+#with open(file_name, 'r') as file:
